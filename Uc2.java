a6bd468c34bf136a9ff3a7fdab483112e65df8fe
pinkul1234 <78338132+pinkul1234@users.noreply.github.com>
2022-05-02 10:45:15 +0530

    Update Uc2.java

--- Uc2.java
+++ Uc2.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
-
 public class Uc2 {
 
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter your firstname and last name");
-            String userName = sc.nextLine();
-            String regularExpression = "^[A-Z][a-z]\\s[A-Z][a-z]{3,}$";
-            if (userName.matches(regularExpression)) {
+            System.out.println("Enter your first name");
+            String userName1 = sc.nextLine();
+            System.out.println("Enter your last name");
+            String userName2 = sc.nextLine();
+            String regularExpressionFirstName = "^[A-Z][a-z]{3,}$";
+            String regularExpressionLastName = "^[A-Z][a-z]{3,}$";
+
+            if (userName1.matches(regularExpressionFirstName)) {
+                if(userName2.matches(regularExpressionLastName)){
+
+                }
                 System.out.println("Valid");
             } else {
                 System.out.println("Invalid");
